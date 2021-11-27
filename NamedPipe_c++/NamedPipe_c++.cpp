066a6feee021121f818d72d3b8866bb3ec9f41dc
Данила Чирков <t343st@gmail.com>
2021-11-27 10:36:17 +0300

    Диалог

--- NamedPipe_c++/NamedPipe_c++.cpp
+++ NamedPipe_c++/NamedPipe_c++.cpp
@@ -5,29 +5,36 @@
 #define PATH_PIPE L"\\\\.\\pipe\\MyPipe"
 
 int main()
-{
-    HANDLE hPipe = CreateNamedPipe(PATH_PIPE, PIPE_ACCESS_DUPLEX,
-        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-        PIPE_UNLIMITED_INSTANCES, 200, 200, INFINITE, NULL);
+{   
     system("chcp 1251");
     printf("Сервер\n");    
     BOOL Connected;
+    HANDLE hPipe;
+    DWORD n;
+    LPWSTR bufferString = (CHAR*)calloc(200, sizeof(CHAR));
+    LPWSTR mass = (CHAR*)calloc(200, sizeof(CHAR));
     while (1)
     {      
-        
+        hPipe = CreateNamedPipe(PATH_PIPE, PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES, 200, 200, INFINITE, NULL);
         Connected = ConnectNamedPipe(hPipe, NULL);
         if (Connected)
         {
             printf("\nПодключение установленно\n");
-            LPWSTR bufferString = (CHAR*)calloc(200, sizeof(CHAR));
-            DWORD n;
-            ReadFile(hPipe, bufferString, 200, &n, NULL);
-            printf(bufferString);
+            if (ReadFile(hPipe, bufferString, 200, &n, NULL))
+            {
+                printf("\nКлиент пишет: %s", bufferString);
+                printf("\nВведите сообщение клиенту: ");
+                gets(mass);
+                WriteFile(hPipe, mass, 200, &n, NULL);
+            }
+          
 
         }
-        
+        CloseHandle(hPipe);
     }
-    CloseHandle(hPipe);
+    
 
 
 
